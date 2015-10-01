81ffa486ed2b367a6e2c0e93c5376086698f8c0e
Ashish Gupta <ashishmgupta@hotmail.com>
2015-09-30 22:37:07 -0400

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace SecurityA2ZWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
